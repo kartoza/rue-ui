ad250258dcdb79ee047439cce593c316cf8a9aef
Dimas <dimas.ciputra@gmail.com>
2025-11-06 11:15:12 +0000

    Kartoza standard structure

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,5 +1,5 @@
 import React, { type ErrorInfo, type ReactNode } from 'react';
-import * as Sentry from "@sentry/react";
+import * as Sentry from '@sentry/react';
 
 interface Props {
   children: ReactNode;
@@ -11,43 +11,43 @@ interface State {
   errorInfo?: ErrorInfo | null;
 }
 
-export default class Index extends React.Component<Props, State> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            hasError: false,
-            errorInfo: null,
-            error: null
-        };
+export default class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      errorInfo: null,
+      error: null,
+    };
+  }
+
+  static getDerivedStateFromError(error: Error): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true, error: error, errorInfo: null };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+    });
+
+    // Send to sentry
+    Sentry.captureException(error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <div>
+          <h1>Something went wrong!</h1>
+          <p>{this.state.error ? this.state.error.message : null}</p>
+        </div>
+      );
     }
 
-    static getDerivedStateFromError(_: Error) : State {
-        // Update state so the next render will show the fallback UI.
-        return {hasError: true, error: _, errorInfo: null};
-    }
-
-    componentDidCatch(error: Error, errorInfo: any) {
-        // You can also log the error to an error reporting service
-        this.setState({
-            error: error,
-            errorInfo: errorInfo
-        })
-
-        // Send to sentry
-        Sentry.captureException(error);
-    }
-
-    render() {
-        if (this.state.hasError) {
-            // You can render any custom fallback UI
-            return <div>
-                <h1>Something went wrong!</h1>
-                <p>
-                    { this.state.error ? this.state.error.message : null }
-                </p>
-            </div>;
-        }
-
-        return this.props.children;
-    }
+    return this.props.children;
+  }
 }
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+import './style.scss';
+
+function Home() {
+  return (
+    <Box p="1rem">
+      <p>Welcome to Kartoza React Base</p>
+    </Box>
+  );
+}
+
+export default Home;
