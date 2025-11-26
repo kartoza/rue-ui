a04de59a2913c0a50b67e33bdebfacfc19a739ae
Irwan Fathurrahman <meomancer@gmail.com>
2025-11-26 13:02:41 +0700

    FIx tests

--- src/components/Toaster/toaster.ts
+++ src/components/Toaster/toaster.ts
@@ -1,12 +1,14 @@
 import { createToaster } from '@chakra-ui/react';
 
-export enum ToasterType {
-  error = 'error',
-  success = 'success',
-  warning = 'warning',
-  info = 'info',
-  loading = 'loading',
-}
+export const ToasterType = {
+  error: 'error',
+  success: 'success',
+  warning: 'warning',
+  info: 'info',
+  loading: 'loading',
+} as const;
+
+export type ToasterType = (typeof ToasterType)[keyof typeof ToasterType];
 
 export const toaster = createToaster({
   placement: 'top-end',
